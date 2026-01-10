b70587305765fb6f95d301b396e77bc6c424d105
jaishikamal <jaishikamal07@gmail.com>
2026-01-10 14:34:47 +0545

    day01

--- src/AdvanceJava/GUI/TwoDShapes/Main2D.java
+++
@@ -1,8 +0,0 @@
-package AdvanceJava.GUI.TwoDShapes;
-
-public class Main2D {
-    public static void main(String[] args) {
-            LineG lineg = new LineG();
-
-    }
-}
--- src/OOP/BasicConcept/Student.java
+++
@@ -1,9 +0,0 @@
-package OOP.BasicConcept;
-
-public class Student {
-
-    // Fields (variables)
-    String name;
-    int age;
-    double weight;
-}
